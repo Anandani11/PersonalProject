ae7754049df2d8b8edde157819dd4a9ecf6239f8
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-04-25 11:30:51 +1200

    site done to progress update stage

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,49 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Quotes for success</h1>
+    <div>
+      <h1>Quotes for success</h1>
 
+      <img src="http://edlester.com/wp-content/uploads/2017/01/4f76cac7d857c1b746417f69cd53b077.png" alt="Quote"></img>
+      <p>
+        <p>
+        Food for your soul
+        </p>
+
+        <p>
+    Ready to face yourself? Do you find yourself in a constant cycle of the same issues? Confused or just Angry all the time? Do you feel that
+     you are  not good enough and always comparing yourself to others? Then read the below:
+        </p>
+
+        <p><font color="Crimson">CHAKRAS:</font></p>
+
+        <p><font color="Purple">What are they?</font></p>
+        <p>
+       It is a Sanskrit hindu term that means circle of light, we have many chakras however our body is composed out of 7 main chakras. Each chakra represents certain emotions which contribute to your quality of life and actions.
+        </p>
+
+        <p>
+          <p><b>Root Chakra know as the Muladhara</b></p>
+          <p><b>Colour:</b> <font color="Red">Red</font></p>
+          <p><b>Represents:</b> Your roots, stabiltiy and security</p>
+          <p><b>Mantra/Sound:</b> LAM</p>
+          <p><b>Element:</b> Earth</p>
+          <p><b>Located:</b> This is your first chakra located at the bottom of your backbone tail</p>
+
+          <p>This chakra would be the first chakra to work on as this represents your base, if you are not balanced and not grounded you will be all over the place. This chakra
+     gives your stability and sense of security.</p>
+        </p>
+
+        <p>
+          Here is a very useful workbook I would recommend to everyone to go through, this here has personally changed my life.
+        </p>
+        <p>
+          <a href="https://www.amazon.com/Chakra-Wisdom-Oracle-Toolkit-Self-Discovery/dp/1780288298">View Workbook here</a>
+        </p>
+
+      </p>
+    </div>
   )
 }
 
-<button>Are you ready?</button>
-
 export default App
-
